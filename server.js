05935bf7851479d3aa1c796645a71edfe4ea24b5
tmosest <tmosest@gmail.com>
2016-10-07 00:45:11 -0400

    Now addes tokens for registered users!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var jwt = require('jwt-simple');
+var moment = require('moment');
 
 var mongoose = require('mongoose');
 var Message = require('./models/message');
@@ -9,29 +11,28 @@ var User = require('./models/user');
 var auth = require('./controllers/auth');
 var messages = require('./controllers/message');
 
+var cors = require('./services/cors');
+var checkAuthorization = require('./services/checkAuthorization');
 
 var serverPort = 5000;
 
+// Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
+// Requests
 app.get('/api/message', messages.get);
-
-app.post('/api/message', messages.post);
-
+app.post('/api/message', checkAuthorization, messages.post);
 app.post('/auth/register', auth.register);
 
+// Connection
 mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
     if(!err) {
         console.log("Connected to Mongo.");
     }
 });
 
+// Run server
 var server = app.listen(serverPort, function() {
     console.log("Server opened on port: ", server.address().port);
 });
